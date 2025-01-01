1d128f23bb5460f3ead0bf9a7db79c22568b7efb
Athul P M <110245493+AthulPM97@users.noreply.github.com>
2025-01-01 10:04:17 +0530

    Update apolloClient.ts

--- src/apollo/apolloClient.ts
+++ src/apollo/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_KEY;
 
 const client = new ApolloClient({
   link: new HttpLink({
